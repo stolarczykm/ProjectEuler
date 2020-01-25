d3e4a48f4ab572c0b33506d3235371b1ca4eff63
Michal Stolarczyk <michal.stolarczyk0@gmail.com>
2020-01-25 11:17:54 +0100

    Problem 32 optimized

--- problems/problem032.cpp
+++ problems/problem032.cpp
@@ -4,24 +4,11 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-
-
-bool has_unique_digits(long a, int n) {
-    auto digits = vector<bool>(n, false);
-    while (a > 0) {
-        int digit = a % 10;
-        if (digit < 1 || digit > n) return false;
-        if (digits[digit-1]) return false;
-        digits[digit-1] = true;
-        a = a / 10;
-    }
-    return true;
-}
-
-bool n_digits(long a) {
+int n_digits(long a) {
     int count = 0; 
     while (a > 0) {
         a = a / 10;
@@ -63,23 +50,31 @@ bool is_pandigital(long a, long b, long c, int n) {
 }
 
 
-
-
 long brute_force(int n) {
-    long sum = 0;
-    for (long i = 1; i < pow(10, n); ++i) {
-        if (!has_unique_digits(i, n)) continue;
+    set<long> products = {};
+    vector<bool> digits_i;
+    vector<bool> digits_j;
+    for (long i = 1; i < pow(10, n/2); ++i) {
         auto i_digits = n_digits(i);
 
-        for (long j = i+1; j <= pow(10, n - i_digits) / i; ++j) {
-            if (!has_unique_digits(j, n)) continue;
-
+        auto min_j_digits = (n /2) - i_digits;
+        auto max_j_digits = ((n+1) / 2) - i_digits;
+        long j_start = max(i+1, (long)pow(10L, min_j_digits - 1));
+        long j_end = (long)pow(10L, max_j_digits); 
+        for (long j = j_start; j < j_end; ++j) {
             long product = i * j;
+
             if (is_pandigital(i, j, product, n)) {
-                sum += product;
+                products.insert(product);
             }
         }
     }
+
+    long sum = 0;
+    for (auto product: products) {
+        sum += product;
+    }
+
     return sum;
 
 }
@@ -93,5 +88,3 @@ int main() {
     // cout << is_pandigital(39, 186, 7254, 9) << endl;
     cout << brute_force(n) << endl;
 }
-
-
